012db37e1d5740926202b69b6e7c9300fff39b87
itsyaadi <aadityabhandari770@gmail.com>
2026-02-11 18:01:50 +0545

    Switch to pure static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  // This is the new way for Tailwind v4
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'static', // or 'server' if you're using Vercel functions
-  adapter: vercel(),
+  output: 'static', 
 });
